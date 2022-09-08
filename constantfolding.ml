aed2b20f63621cfca2aee1ee7715ccbde4d217d7
Austin Copley <acopley@mines.edu>
2022-09-08 16:02:37 -0600

    finished constfold

--- constantfolding.ml
+++ constantfolding.ml
@@ -14,7 +14,12 @@ type rec constfold = fun e -> match e with
   | Number(n) -> e
   | Parenthesized(e) -> Parenthesized(constfold(e))
   | BinaryOp(l, op, r) -> match (constfold l, constfold r) with
-    | bungle
+    | (Number(a), Number(b)) -> (match op with
+      | Plus -> Number (a + b)
+      | Multiply -> Number(a * b)
+      | Minus -> Number(a - b)
+    )
+    | (a, b) -> BinaryOp(a, op, b)
 in constfold (
   BinaryOp(Identifier("x"), Plus, Parenthesized(BinaryOp(Number 2, Minus, Number 1)))
 );;
